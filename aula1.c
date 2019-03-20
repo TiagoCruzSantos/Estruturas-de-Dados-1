87bda84eab394b3e5674738b0ab7074f38da156c
Tiago Cruz <tiago.unity.cruz@gmail.com>
2019-03-20 16:29:14 -0300

    aula 1

--- aula1.c
+++ aula1.c
@@ -25,15 +25,29 @@ void ex2(void *a, void *b){
     }
 }
 
-void ex3(){
-    
+int trocaInteiros(int *a, int *b){
+   int help;
+   
+   if(*a > *b){
+       help = *a;
+       *a = *b;
+       *b = help;
+       return 1;
+   }else{
+       return 0;
+   }
 }
 
 int main(){
     ex1();
-    int var1;
-    int var2;
+    int var1 = 5;
+    int var2 = 4;
     ex2(&var1, &var2);
-    printf("%p, %p\n", &var1, &var2);
+    if(trocaInteiros(&var1, &var2)){
+        printf("TROCADO: %d %d\n", var1, var2);
+    }else{
+        printf("ORIGINAL: %d %d\n", var1, var2);
+    }
+
     return 0;
 }
